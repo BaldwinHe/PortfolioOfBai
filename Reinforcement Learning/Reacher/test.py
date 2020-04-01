2bc29451a103a4c341ee571d644c7ad4b0baa183
BaldwinHe <1103108201@qq.com>
2020-04-01 23:20:45 +0800

    update test.py

--- Reinforcement Learning/Reacher/test.py
+++ Reinforcement Learning/Reacher/test.py
@@ -1,12 +1,12 @@
 import sys
 
 from unityagents import UnityEnvironment
-
+import numpy as np
 from agent import *
 from config import *
 
 if __name__ == '__main__':
-    agent = Agent(33, 4, 1)
+    agent = Agent(33,4,1,args.seed)
     agent.load(args.actor, args.critic)
     env = UnityEnvironment(file_name=args.unity)
 
@@ -14,19 +14,17 @@ if __name__ == '__main__':
     brain = env.brains[brain_name]
 
     env_info = env.reset(train_mode=False)[brain_name]  # reset the environment
-    state = env_info.vector_observations[0]  # get the current state
-    score = 0  # initialize the score
+    states = env_info.vector_observations
+    scores = 0  # initialize the score
     t_max = 1000
     while t_max > 0:
-        action = agent.act(state) # select an action
-        env_info = env.step(action)[brain_name]  # send the action to the environment
-        next_state = env_info.vector_observations[0]  # get the next state
-        reward = env_info.rewards[0]  # get the reward
-        done = env_info.local_done[0]  # see if episode has finished
-        score += reward  # update the score
-        state = next_state  # roll over the state to next time step
-        print("{}-Score: {}".format(t_max, score))
+        actions = agent.act(states) # select an action
+        env_info = env.step(actions)[brain_name]  # send the action to the environment
+        next_states = env_info.vector_observations  # get the next state
+        rewards = env_info.rewards[0]  # get the reward
+        dones = env_info.local_done  # see if episode has finished
+        scores += rewards  # update the score
+        states = next_states  # roll over the state to next time step
+        print("{}-Score: {}".format(t_max, np.mean(scores)))
         sys.stdout.flush()
         t_max -= 1
-        if done:  # exit loop if episode finished
-            break
